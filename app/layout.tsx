ae74ef0347c76973398989376f1822b4a9ed5afb
allash <1172129+allash@users.noreply.github.com>
2024-08-29 23:35:44 +0200

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,29 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <div className="flex h-screen bg-gray-100">
-          <div className="hidden w-64 flex-col bg-gray-800 md:flex">
-            <div className="flex h-16 items-center justify-center bg-gray-900">
-              <span className="font-bold uppercase text-white">
-                MediaViewer
-              </span>
-            </div>
-            <div className="flex flex-1 flex-col overflow-y-auto">
-              <nav className="flex-1 bg-gray-800 px-2 py-4">
-                <a
-                  href="#"
-                  className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
-                >
-                  Media 1
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
-                >
-                  Media 2
-                </a>
-              </nav>
-            </div>
-          </div>
+          <SideNav />
 
           <div className="flex flex-1 flex-col overflow-y-auto">
             <div className="p-12">
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -36,7 +36,7 @@ export default function NavLinks() {
               pathname: '/',
               query: { video: `${link.videoId}` },
             }}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
             {/* <LinkIcon className="w-6" /> */}
             <p className="hidden md:block">{link.name}</p>
