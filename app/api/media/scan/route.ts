929938e42f220a4576db0f034d61517fb04dfc37
allash <1172129+allash@users.noreply.github.com>
2024-09-10 21:59:42 +0200

    prepare docker image

--- app/api/media/scan/route.ts
+++ app/api/media/scan/route.ts
@@ -1,13 +1,14 @@
 import findFiles from '@/lib/findFiles';
 import { FileInfo } from '@/models/fileInfo';
 import { NextRequest, NextResponse } from 'next/server';
+import path from 'path';
 
 const ignoredFiles = ['.DS_Store', '.gitkeep'];
 
 export async function GET(request: NextRequest) {
-  const items: FileInfo[] = findFiles('public/collections').filter(
+  const collectionsDir = path.join(process.cwd(), 'public', 'collections');
+  const items: FileInfo[] = findFiles(collectionsDir).filter(
     (e) => !ignoredFiles.includes(e.name),
   );
-
   return NextResponse.json(items);
 }
